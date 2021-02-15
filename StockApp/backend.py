728b1eb3ed4bcd686708c6eebddbd1494258136d
JCamyre <JWcamry03@gmail.com>
2021-02-15 14:56:53 -0800

    Fixed issues.

--- StockApp/backend.py
+++ StockApp/backend.py
@@ -16,14 +16,14 @@ HEADERS = {'User-Agent': "'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.3
 def _price_target(ticker, exchange='NASDAQ'):
 	BASE_URL = f'https://www.marketbeat.com/stocks/{exchange}/{ticker}/price-target/'
 	response = get(BASE_URL, headers=HEADERS, timeout=20)
-	assert response.status_code == 200
+
 	soup = BeautifulSoup(response.content, 'lxml')
 	table = soup.find('table', {'class': "scroll-table"})
 	# price_target = soup.find('table', {'class': 'scroll-table'})
 	_pattern = re.compile(r'Price Target: \$\d{1,3}\.\d\d')
 	return _find_match(_pattern, table.get_text()).group(0)
 
-print(_price_target('AAPL'))
+print(_price_target('A', exchange='NYSE'))
 
 # html = soup.prettify("utf-8") Good way to visualize what your Python code is visualizing
 # with open('output1.html', 'w', encoding='utf-8') as f:
