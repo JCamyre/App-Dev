75957802a3b105c42765a05410fc63eda6bbf8a5
JCamyre <JWcamry03@gmail.com>
2021-02-15 14:08:20 -0800

    .

--- StockApp/backend.py
+++ StockApp/backend.py
@@ -1,5 +1,10 @@
 from requests import get
 from bs4 import BeautifulSoup
+import re
+
+def _no_attributes(tag):
+	if 'td' in str(tag):
+		return tag.has_attr('class') or tag.has_attr('id')
 
 HEADERS = {'User-Agent': "'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 " # Telling the website what browser I am "using"
 							 "(KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36'"}
@@ -9,11 +14,13 @@ def _price_target(ticker, exchange='NASDAQ'):
 	response = get(BASE_URL, headers=HEADERS, timeout=20)
 	assert response.status_code == 200
 	soup = BeautifulSoup(response.content, 'lxml')
-	price_target = soup.find('table', {'class': 'scroll-table'})
-	return price_target
+	table = soup.find('table', {'class': "scroll-table"})
+	# price_target = soup.find('table', {'class': 'scroll-table'})
+	_pattern = re.compile(r'Price Target: \$\d{1,3}\.\d\d')
+	print([i.get_text() for i in table.find_all('td')], table.get_text())
+	return table.find_all(_no_attributes)
 
 print(_price_target('AAPL'))
-
 
 # html = soup.prettify("utf-8") Good way to visualize what your Python code is visualizing
 # with open('output1.html', 'w', encoding='utf-8') as f:
